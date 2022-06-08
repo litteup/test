6391c0b9369f6e700469e93d3ee8cdd63f32efba
light <roradobo@gmail.com>
2022-06-08 23:53:15 +0100

    third commit

--- appone/admin.py
+++ appone/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from . models import People, Address, Product, Bio#, Doctor
+from . models import People, Address, Product, Bio, Doctor
 
 # Register your models here.
 admin.site.register(People)
 admin.site.register(Address)
-#admin.site.register(Doctor)
+admin.site.register(Doctor)
 admin.site.register(Product)
 admin.site.register(Bio)
